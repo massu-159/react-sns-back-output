8ae07ea2e60905f2fee63e88a4d065aeab50d913
まっすー <mgajtwjp@gmail.com>
2022-10-05 19:18:53 +0900

    use Node.js(express)

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postsRoute = require("./routes/posts");
+const PORT = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// データベース接続
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+  console.log("DBと接続中");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// ミドルウェア
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postsRoute);
+
+app.listen(PORT, () => console.log("サーバーが起動しました"));
